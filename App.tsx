3ed1ff484b6225df7914f21d1e6d694cc795dbe7
mayromyller <deehmyller11@gmail.com>
2024-07-10 22:46:44 -0300

    feat: Update App component to use ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import 'react-native-gesture-handler';
 
-import RootStack from './navigation';
+import RootStack from './src/routes';
+
+import { ThemeProvider } from '~/theme/context/ThemeContext';
 
 export default function App() {
-  return <RootStack />;
+  return (
+    <ThemeProvider>
+      <RootStack />
+    </ThemeProvider>
+  );
 }
