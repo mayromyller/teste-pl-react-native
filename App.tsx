6a2f71532ec504a6dd4aa60860cc72fc64fd0aca
mayromyller <deehmyller11@gmail.com>
2024-07-12 14:15:10 -0300

    feat: Add Google Fonts Inter and SplashScreen functionality

--- App.tsx
+++ App.tsx
@@ -1,13 +1,43 @@
 import 'react-native-gesture-handler';
 
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import RootStack from './src/routes';
 
 import { ThemeProvider } from '~/theme/context/ThemeContext';
 
 export default function App() {
+  const [fontsLoaded] = Font.useFonts({
+    Inter_Regular: Inter_400Regular,
+    Inter_Medium: Inter_500Medium,
+    Inter_SemiBold: Inter_600SemiBold,
+    Inter_Bold: Inter_700Bold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <ThemeProvider>
-      <RootStack />
-    </ThemeProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <ThemeProvider>
+        <RootStack />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
